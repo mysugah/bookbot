695fed4653bf166fdf531313c5b4aed8bdde9f7b
root <root@DESKTOP-VAUVS9S.localdomain>
2025-12-20 15:48:14 -0800

    adding of stats.py and updating functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+from stats import split_n_count, each_char_count
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    #print(get_book_text("/root/bookbot/books/frankenstein.txt"))
+    book = get_book_text("/root/bookbot/books/frankenstein.txt")
+    split_n_count(book)
+    each_char_count(book)
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,15 @@
+def split_n_count(string):
+    words = string.split()
+    return print(f"Found {len(words)} total words")
+
+def each_char_count(string):
+    lowercase = string.lower()
+    str_to_int = {}
+
+    for character in lowercase:
+        if character in str_to_int:
+            str_to_int[character] += 1
+        else:
+            str_to_int[character] = 1
+
+    return print(str_to_int)
